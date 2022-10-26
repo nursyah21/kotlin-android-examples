294cb8183f0a547740c773a19eaa99336f6a6be4
nursyahjaya <nursyahjaya@gmail.com>
2022-10-27 04:12:54 +0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # kotlin-android-examples
-list my android examples with kotlin
+
+list my simple android examples with kotlin based on google codelabs
+
+---
+
+[android-navigation-examples](/examples/android-navigation-fragments) - [google-codelabs](https://developer.android.com/codelabs/basic-android-kotlin-training-fragments-navigation-component)
+
+<details><summary>see screenshot android navigation fragments</summary>
+  <img src="/pictures/navbar.png" />
+</details>
---
+++ examples/android-navigation-fragments/app/src/main/java/com/nursyah/learnbottomnavigation/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.nursyah.learnbottomnavigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.nursyah.learnbottomnavigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityMainBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    val navView: BottomNavigationView = binding.navView
+    val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+    val navController = navHostFragment.navController
+
+    val appBarConfiguration = AppBarConfiguration(
+      setOf(
+        R.id.navigation_home,
+        R.id.navigation_dashboard
+      )
+    )
+    setupActionBarWithNavController(navController, appBarConfiguration)
+    navView.setupWithNavController(navController)
+  }
+}
